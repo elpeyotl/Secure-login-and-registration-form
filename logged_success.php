57dd815e1251c5420a2adcb6a7bce9cc2cdeaf22
Jelmer de Jong <jelmerjtdejong@gmail.com>
2013-07-30 10:10:03 +0200

    Added login/session check to success page.
    Added the check if someone is logged in to logged_success.php, the code can be reused on other pages, making a page secure and only available for logged in users.

--- logged_success.php
+++ logged_success.php
@@ -1,4 +1,10 @@
-    <!DOCTYPE html>
+<?php
+include 'secure/db_connect.php';
+include 'secure/functions.php';
+sec_session_start(); // Our custom secure way of starting a php session.
+if(login_check($mysqli) == true) { ?>
+
+<!DOCTYPE html>
     <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -45,3 +51,9 @@
 
 </body>
 </html>
+
+<?php } else {
+   echo 'You are not authorized to access this page, please login. <br/>';
+}
+
+;?>
